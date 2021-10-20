d94cf5099e350633fd2801308c0d551c7d821071
Owen Roth <69156111+oroth8@users.noreply.github.com>
2021-10-20 16:29:07 -0500

    added env vars hidden

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'simple_form'
 gem 'faker'
 gem 'devise'
 gem 'friendly_id', '~> 5.4.0'
+gem 'figaro'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/environment.rb
+++ config/environment.rb
@@ -5,10 +5,10 @@ require_relative "application"
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-    :port           => ENV['465'],
-    :address        => ENV['smtp.mailgun.org'],
-    :user_name      => ENV['owen@sandboxa13301e0a561430783039401f32c0445.mailgun.org'],
-    :password       => ENV['2c1261c3f88c958b92b3b5b461b99c81-2bf328a5-e1687954'],
+    :port           => ENV['SMTP_PORT'],
+    :address        => ENV['SMTP_ADDRESS'],
+    :user_name      => ENV['SMTP_LOGIN'],
+    :password       => ENV['SMTP_PASSWORD'],
     :domain         => 'safe-anchorage-69988.herokuapp.com',
     :authentication => :plain,
   }
