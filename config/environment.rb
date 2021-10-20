4e37c9d3da8c274e3844935848b9fb24ad7bee1b
Owen Roth <69156111+oroth8@users.noreply.github.com>
2021-10-20 16:42:10 -0500

    changed env vars

--- config/environment.rb
+++ config/environment.rb
@@ -5,10 +5,10 @@ require_relative "application"
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-    :port           => ENV['SMTP_PORT'],
-    :address        => ENV['SMTP_ADDRESS'],
-    :user_name      => ENV['SMTP_LOGIN'],
-    :password       => ENV['SMTP_PASSWORD'],
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
     :domain         => 'safe-anchorage-69988.herokuapp.com',
     :authentication => :plain,
   }
