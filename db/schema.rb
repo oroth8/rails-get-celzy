53d1bfe46a2ddcff2ada945dc5c8370ef36d0ab6
Owen Roth <69156111+oroth8@users.noreply.github.com>
2021-10-21 15:10:07 -0500

    added fields to courses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_20_200822) do
+ActiveRecord::Schema.define(version: 2021_10_21_194523) do
 
   create_table "courses", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,10 @@ ActiveRecord::Schema.define(version: 2021_10_20_200822) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
     t.string "slug"
+    t.text "short_description"
+    t.string "language", default: "English", null: false
+    t.string "level", default: "Beginner", null: false
+    t.integer "price", default: 0, null: false
     t.index ["slug"], name: "index_courses_on_slug", unique: true
     t.index ["user_id"], name: "index_courses_on_user_id"
   end
