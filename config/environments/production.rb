54c0fc3a67d37cc818bac97a44b8b48fdfdb03b3
Owen Roth <69156111+oroth8@users.noreply.github.com>
2021-10-21 11:41:57 -0500

    Added AWS SMTP Settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,8 +65,20 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
+  
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = {host: "safe-anchorage-69988.herokuapp.com", protocol: "https"}
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  port: 587,
+  address: 'email-smtp.us-east-2.amazonaws.com',
+  user_name: ENV['SMTP_CREDENTIALS_USER_NAME'],
+  password: ENV['SMTP_CREDENTIALS_PASSWORD'],
+  authentication: :plain,
+  enable_starttls_auto: true
+}
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
