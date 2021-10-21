20db1604bcec56847cec6f11efa9b01a2f0c42ed
Owen Roth <69156111+oroth8@users.noreply.github.com>
2021-10-21 11:49:41 -0500

    action mail cache set to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "rails_get_celzy_production"
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   
