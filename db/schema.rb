ae4fcbe9e5279ea934d36b03a02bbbfd1fe398f9
Owen Roth <69156111+oroth8@users.noreply.github.com>
2021-10-22 14:25:37 -0500

    added friend_id to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_22_154754) do
+ActiveRecord::Schema.define(version: 2021_10_22_192123) do
 
   create_table "activities", force: :cascade do |t|
     t.string "trackable_type"
@@ -85,9 +85,11 @@ ActiveRecord::Schema.define(version: 2021_10_22_154754) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "slug"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["slug"], name: "index_users_on_slug", unique: true
   end
 
   create_table "users_roles", id: false, force: :cascade do |t|
